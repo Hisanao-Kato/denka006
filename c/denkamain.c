0fd5a21415d657c36a2cab2c08483011fa0c38ff
Hisanao Kato <kato@kato-development.com>
2017-05-27 01:04:28 +0900

    First Commit

---
+++ c/denkamain.c
@@ -0,0 +1,16 @@
+/**
+ * denkamain.c
+ * -------------------
+ * Denka main source file.
+ *
+ */
+
+// Include Files
+#include "denkamain.h"
+
+// function prototiping
+int main(void);
+
+int main(void){
+
+}
---
+++ h/denkamain.h
@@ -0,0 +1,14 @@
+#ifndef _DENKAMAIN_H
+#define _DENKAMAIN_H
+
+/**
+ * denkamain.h
+ * -------------------
+ * Denka main header file.
+ *
+ */
+
+ //Include Files
+
+
+#endif
